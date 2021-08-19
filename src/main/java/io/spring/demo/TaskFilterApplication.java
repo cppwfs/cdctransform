89a8a618c6908266917ba4ffc97b3ae6f59b2b97
Glenn Renfro <grenfro@pivotal.io>
2021-08-18 20:14:32 -0400

    Updated app to simplify function

--- src/main/java/io/spring/demo/TaskFilterApplication.java
+++ src/main/java/io/spring/demo/TaskFilterApplication.java
@@ -1,9 +1,6 @@
 package io.spring.demo;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.Date;
-import java.util.Locale;
 import java.util.function.Function;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -28,85 +25,115 @@ public class TaskFilterApplication {
 
 	@Bean
 	public Function<String, String> transform() {
-		return  (payload) -> {
-			ObjectMapper objectMapper = new ObjectMapper();
-			JsonNode jsonNode = null;
-			try {
-				jsonNode = objectMapper.readTree(payload);
-			}
-			catch (JsonProcessingException e) {
-				throw new IllegalStateException(e);
-			}
-			String result = "";
+		return (payload) -> {
+			JsonNode jsonNode = getJsonNode(payload);
+			String result;
 			String tableName = jsonNode.get("source").get("table").asText();
-			if(tableName == null || jsonNode.get("after") == null) {
-				return "";
+			if (tableName == null || jsonNode.get("after") == null) {
+				result = translateOthers(jsonNode);
 			}
-			if(tableName != null && tableName.equals("plans")) {
-				Plan plan = new Plan(jsonNode.get("after").get("plan_id").asInt(-1),
-						jsonNode.get("after").get("plan_name").asText(),
-						jsonNode.get("after").get("data_price").asDouble(),
-						jsonNode.get("after").get("data_price").asDouble());
-				try {
-					result = objectMapper.writeValueAsString(plan);
+			else {
+				if (tableName.equals("plans")) {
+					result = translatePlan(jsonNode);
 				}
-				catch (JsonProcessingException e) {
-					e.printStackTrace();
-					result = "******  FAILED    " + payload;
+				else if (tableName.equals("users")) {
+					result = translateUser(jsonNode);
 				}
-			}
-			if(tableName != null && tableName.equals("users")) {
-				User user = new User(jsonNode.get("after").get("id").asInt(-1),
-						jsonNode.get("after").get("last_name").asText(),
-						jsonNode.get("after").get("first_name").asText(),
-						jsonNode.get("after").get("plan_id").asInt(-1));
-				try {
-					result = objectMapper.writeValueAsString(user);
+				else if (tableName.equals("call_usage")) {
+					result = translateCallUsage(jsonNode);
 				}
-				catch (JsonProcessingException e) {
-					e.printStackTrace();
-					result = ""; //"******  FAILED    " + payload;
+				else if (tableName.equals("data_usage")) {
+					result = translateDataUsage(jsonNode);
+				}
+				else {
+					result = translateOthers(jsonNode);
 				}
 			}
-			System.out.println("******* >" +tableName);
-			if(tableName != null && tableName.equals("call_usage")) {
-				SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-DD hh:mm:ss", Locale.ENGLISH);
-				try {
-				CallUsage usage = new CallUsage(jsonNode.get("after").get("usage_in_minutes").asDouble(),
-						new Date(jsonNode.get("after").get("date").asLong()),
-						jsonNode.get("after").get("user_id").asInt(-1));
-					result = objectMapper.writeValueAsString(usage);
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-					result = ""; //"******  FAILED    " + payload;
-				}
-			}
-			if(tableName != null && tableName.equals("data_usage")) {
-				try {
-					DataUsage usage = new DataUsage(jsonNode.get("after").get("usage_in_bytes").asDouble(),
-							new Date(jsonNode.get("after").get("date").asLong()),
-							jsonNode.get("after").get("user_id").asInt(-1));
-
-					result = objectMapper.writeValueAsString(usage);
-
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-
-					result = ""; //"******  FAILED    " + payload;
-				}
-			}
-//			else {
-//				result = "************ " + tableName + " ==> " + payload;
-//			}
-//			String result = null;
-//			if(payload.contains("\"exitCode\":2")) {
-//				JdbcTemplate template = new JdbcTemplate(dataSource);
-//				template.execute("truncate table task_status");
-//				result = "{\"name\":\""+ taskName + "\"}";
-//			}
 			return result;
 		};
+	}
+
+	public String translateUser(JsonNode jsonNode) {
+		String result;
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		User user = new User(jsonNode.get("after").get("id").asInt(-1),
+				jsonNode.get("after").get("last_name").asText(),
+				jsonNode.get("after").get("first_name").asText(),
+				jsonNode.get("after").get("plan_id").asInt(-1));
+		try {
+			result = objectMapper.writeValueAsString(user);
+		}
+		catch (JsonProcessingException e) {
+			e.printStackTrace();
+			result = "";
+		}
+		return result;
+	}
+
+	public String translatePlan(JsonNode jsonNode) {
+		String result;
+		ObjectMapper objectMapper = new ObjectMapper();
+		Plan plan = new Plan(jsonNode.get("after").get("plan_id").asInt(-1),
+				jsonNode.get("after").get("plan_name").asText(),
+				jsonNode.get("after").get("data_price").asDouble(),
+				jsonNode.get("after").get("data_price").asDouble());
+		try {
+			result = objectMapper.writeValueAsString(plan);
+		}
+		catch (JsonProcessingException e) {
+			e.printStackTrace();
+			result = "";
+		}
+		return result;
+	}
+
+	public String translateCallUsage(JsonNode jsonNode) {
+		String result;
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			CallUsage usage = new CallUsage(jsonNode.get("after").get("usage_in_minutes").asDouble(),
+					new Date(jsonNode.get("after").get("date").asLong()),
+					jsonNode.get("after").get("user_id").asInt(-1));
+			result = objectMapper.writeValueAsString(usage);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			result = "";
+		}
+		return result;
+	}
+
+	public String translateDataUsage(JsonNode jsonNode) {
+		String result;
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+
+			DataUsage usage = new DataUsage(jsonNode.get("after").get("usage_in_bytes").asDouble(),
+					new Date(jsonNode.get("after").get("date").asLong()),
+					jsonNode.get("after").get("user_id").asInt(-1));
+			result = objectMapper.writeValueAsString(usage);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			result = "";
+		}
+		return result;
+	}
+
+	public String translateOthers(JsonNode jsonNode) {
+		return "";
+	}
+	public JsonNode getJsonNode(String payload) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode jsonNode = null;
+		String result;
+		try {
+			jsonNode = objectMapper.readTree(payload);
+		}
+		catch (JsonProcessingException e) {
+			throw new IllegalStateException(e);
+		}
+		return jsonNode;
 	}
 }
